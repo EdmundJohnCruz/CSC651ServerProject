2fa7ddb31b8856efe61956923a8cd6e7ceb2741f
begumsakin112 <bsakin@mail.sfs.edu>
2021-04-22 23:54:26 +0300

    configured the data base and added tables

--- app/webApp/webApp/settings.py
+++ app/webApp/webApp/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = 'webApp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/begum/Documents/project/CSC651ServerProject/app/webApp/my.cnf',
+        },
     }
 }
 
