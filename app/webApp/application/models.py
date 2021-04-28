1419cb67f1d1e961c7f24be8c7c4f0d841e932f7
begumsakin112 <bsakin@mail.sfs.edu>
2021-04-29 00:57:19 +0300

    fixed the home directory issue and as an example added a component on reporter table , it is inside view.py .

--- app/webApp/application/models.py
+++ app/webApp/application/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Reporter(models.Model):
     full_name = models.CharField(max_length=70)
-
+    
     def __str__(self):
         return self.full_name
 
@@ -10,7 +10,6 @@ class Article(models.Model):
     pub_date = models.DateField()
     headline = models.CharField(max_length=200)
     content = models.TextField()
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.headline
--- app/webApp/application/views.py
+++ app/webApp/application/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import Reporter, Article 
+from django.utils import timezone
 
 # Create your views here.
+# localhost:3000/index
+
 def index(request):
+  reporter1 = Reporter(full_name='Begum')
+  artical1 = Article(headline='test_headline', content='testtt', pub_date=timezone.now())
+  reporter1.save()
+  artical1.save()
   return render(request, 'home/index.html')
 
+# localhost:3000/aboutUs
 def aboutUs(request):
   return render(request, 'home/aboutUs.html')
+  
+
+  
+  
+  
+  
